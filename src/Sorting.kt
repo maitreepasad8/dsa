b0729def79cd70a38d667aeb847bb27c62cb7c84
Maitree Pasad <maitree@dashtoon.com>
2024-07-29 09:57:28 +0530

    quick sort

--- src/Sorting.kt
+++ src/Sorting.kt
@@ -47,15 +47,12 @@ class Sorting {
         }
     }
 
-    fun mergeSort(nums: IntArray) {
-        sort(nums, 0, nums.size - 1)
-    }
 
-    private fun sort(a: IntArray, l: Int, r: Int) {
+    fun mergeSort(a: IntArray, l: Int, r: Int) {
         if (l < r) {
             val m = l + (r-l)/2
-            sort(a, l, m)
-            sort(a, m+1, r)
+            mergeSort(a, l, m)
+            mergeSort(a, m+1, r)
             merge(a, l, m, r)
         }
     }
@@ -81,12 +78,33 @@ class Sorting {
             a[k++] = b[j++]
         }
     }
+
+    fun quickSort(a: IntArray, l: Int, r: Int) {
+        if (l<r) {
+            val pivot = partition(a, l, r)
+            quickSort(a, l, pivot - 1)
+            quickSort(a, pivot + 1, r)
+        }
+    }
+
+    private fun partition(a: IntArray, l: Int, r: Int): Int {
+        val pivot = a[r]
+        var i = l
+        for (j in i..<r){
+            if (a[j] < pivot){
+                swap(a, i++, j)
+            }
+        }
+        swap(a, i, r)
+
+        return  i
+    }
 }
 
 fun main () {
     val sorting = Sorting()
-    val nums = intArrayOf(8,4,3,2,9,6)
-    sorting.mergeSort(nums)
+    val nums = intArrayOf(8,4,3,2,9)
+    sorting.quickSort(nums, 0, nums.size - 1)
     printArray(nums)
 
 }
